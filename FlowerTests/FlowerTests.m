9daf534654629b4e5e518b7f94bbe1e62a53a6c9
Nick Blackwell <nickblackwell82@gmail.com>
2015-06-02 08:27:03 -0700

    fake test, for ci trigger

--- FlowerTests/FlowerTests.m
+++ FlowerTests/FlowerTests.m
@@ -28,7 +28,8 @@
 
 - (void)testExample
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    XCTAssertTrue(true, @"Pass");
+    //XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
 }
 
 @end
