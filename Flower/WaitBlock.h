246e498457a934f58b8d8beec660c996a35e6192
nickolanack <nickblackwell82@gmail.com>
2015-06-04 12:06:04 -0700

    added wait for signal block, pauses execution until boolean input pin becomes positive.

---
+++ Flower/WaitBlock.h
@@ -0,0 +1,13 @@
+//
+//  WaitBlock.h
+//  Flower
+//
+//  Created by Nick Blackwell on 2015-06-04.
+//  Copyright (c) 2015 Nick Blackwell. All rights reserved.
+//
+
+#import "FunctionalBlock.h"
+
+@interface WaitBlock : FunctionalBlock
+
+@end
---
+++ Flower/WaitBlock.m
@@ -0,0 +1,82 @@
+//
+//  WaitBlock.m
+//  Flower
+//
+//  Created by Nick Blackwell on 2015-06-04.
+//  Copyright (c) 2015 Nick Blackwell. All rights reserved.
+//
+
+#import "WaitBlock.h"
+#import "VariableConnection.h"
+#import "BooleanVariable.h"
+
+
+@interface WaitBlock()
+
+@property NSCondition *lock;
+@property bool unlocked;
+
+@end
+
+
+@implementation WaitBlock
+
+-(void)configure{
+    
+    [super configure];
+    
+    VariableConnection *signal=[[VariableConnection alloc] init];
+    [signal setConnectionAnchorTypeDestination:ConnectionEndPointAnchorTypeTop];
+    [signal setConnectionAnchorTypeSource:ConnectionEndPointAnchorTypeTop];
+    [signal setCenterAlignOffsetDestination:CGPointMake(-15, 0)];
+    [signal connectNode:nil toNode:self];
+    
+    [signal setVariableType:[BooleanVariable class]];
+    [signal setMidPointColor:[UIColor blueColor]];
+    
+    
+    [self setName:@"Wait For Signal"];
+    
+}
+
+
+-(JSValue *)blockEvaluateContext:(JSContext *)context withPreviousBlock:(FunctionalBlock *)block{
+    JSValue *output=[super blockEvaluateContext:context withPreviousBlock:block];
+    _unlocked=false;
+    _lock=[NSCondition new];
+    
+    bool wait=true;
+    
+    Block *signal=((VariableConnection *)[self.inputVariableConnections objectAtIndex:0]).source;
+    if(signal!=nil&&[signal isKindOfClass:[BooleanVariable class]]){
+        wait =![[((BooleanVariable *)signal) value] boolValue];
+    }
+    
+    
+    
+    
+    if(wait){
+        [_lock lock];
+    }
+    while (!_unlocked)[_lock wait];
+    [_lock unlock];
+    return output;
+}
+
+-(void)notifyInputVariableDidChangeValueForConnection:(VariableConnection *)vc{
+    bool wait=true;
+    Block *signal=((VariableConnection *)[self.inputVariableConnections objectAtIndex:0]).source;
+    if(signal!=nil&&[signal isKindOfClass:[BooleanVariable class]]){
+        wait =![[((BooleanVariable *)signal) value] boolValue];
+    }
+    
+    if(!wait){
+        [_lock lock];
+        _unlocked=true;
+        [_lock signal];
+        [_lock unlock];
+    }
+    
+}
+
+@end
